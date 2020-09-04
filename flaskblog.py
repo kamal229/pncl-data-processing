06674e0b029c43d757c7a9357eec35701d000091
ostrich893 <ksingh15@illinois.edu>
2020-09-03 20:46:18 -0500

    changed app

--- flaskblog.py
+++ flaskblog.py
@@ -19,12 +19,12 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello():
-	return render_template('home.html', posts = posts)
+def home():
+	return "<h1>Home Page</h1>"
 
-@app.route("/about")
-def about():
-	return render_template('about.html', title = 'About')
+# @app.route("/about")
+# def about():
+# 	return render_template('about.html', title = 'About')
 
 if __name__ == '__main__':
 	app.run(debug=True)
