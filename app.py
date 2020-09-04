9b845392cd11d7bf5fc9cce336168d79bdb28f1d
ostrich893 <ksingh15@illinois.edu>
2020-09-03 20:57:28 -0500

    changed Procfile

--- app.py
+++ app.py
@@ -20,11 +20,11 @@ posts = [
 @app.route("/")
 @app.route("/home")
 def home():
-	return "<h1>Home Page</h1>"
+	return render_template('home.html', posts = posts)
 
-# @app.route("/about")
-# def about():
-# 	return render_template('about.html', title = 'About')
+@app.route("/about")
+def about():
+	return render_template('about.html', title = 'About')
 
 if __name__ == '__main__':
 	app.run(debug=True)
